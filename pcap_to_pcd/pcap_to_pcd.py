02c56b2f798f4a0f5c9d2205ff00aeb98f7941a5
Ben Kruse <itsbenkruse@gmail.com>
2022-06-10 17:18:25 -0700

    Added batch functionality. Error with multiprocessing.

--- pcap_to_pcd/pcap_to_pcd.py
+++ pcap_to_pcd/pcap_to_pcd.py
@@ -9,7 +9,7 @@ import sys
 import os
 import subprocess
 import time
-
+from multiprocessing import Pool, freeze_support, Process
 
 def pcap_to_bag(pcap_file_name: str, number_of_sensors: int) -> None:
     ''' Generates a bag file containing the pcap data to use in other steps
@@ -71,40 +71,105 @@ def bag_to_pcd(pc_bag_name):
     subprocess.run(f"rosrun pcl_ros bag_to_pcd {os.path.abspath(pc_bag_name)} /os_cloud_node/points ./temp/{new_folder_name}", shell=True)
     
 
-
-def main():
-    ''' Command line arguments:
-        pcap file name
-        json file name
-        number of sensors (16, 32, 64, 128)
-            This is usually part of the pcap name
+def batch_get_file_names(input_folder):
     '''
-    
-    if len(sys.argv) < 4:
-            print("Must have pcap name, json file, and number of sensors as arguments")
-            return 1
-    pcap_file_name = sys.argv[1]
-    json_file_name = sys.argv[2]
-    number_of_sensors = sys.argv[3]
-    
-    # Source ros setup file (. is used for the shell python uses to source)
-    subprocess.run('source /opt/ros/noetic/setup.sh', shell=True)
+    '''
+    onlyfiles = [f for f in os.listdir(input_folder) if os.path.isfile(input_folder + f)]
+    pcaps = [os.path.splitext(f)[0] for f in onlyfiles if os.path.splitext(f)[1] == ".pcap"]
+    for pcap in pcaps:
+        if not os.path.exists(input_folder + pcap + ".json"):
+            print(f"pcap file {pcap}.pcap does not have a matching json")
+            return None
+    return pcaps
 
+
+def run_batch_conversions(input_folder, file_names, sensor_count):
+    '''
+    '''
+    file_count = len(file_names)
+    file_tuples = []
+    for filename in file_names:
+        pcap_name = input_folder + filename + ".pcap"
+        json_name = input_folder + filename + ".json"
+        file_tuples.append((pcap_name, json_name, sensor_count))
+    if __name__ == "__main__":
+        pool = Pool() 
+    print("this worked")
+
+    pool.starmap(run_conversion_test, file_tuples) 
+
+    pool.join()
+
+
+def run_conversion_test(pcap_file_name, json_file_name, number_of_sensors):
+    print(f"Running conversions for {pcap_file_name}, {json_file_name}, {number_of_sensors}")
+    time.sleep(5)
+    print(f"Conversions done for {pcap_file_name}")
+
+def run_conversion(pcap_file_name, json_file_name, number_of_sensors):
+    '''
+    '''
     # Converting the pcap file to a bag file
     pcap_bag_name = pcap_to_bag(pcap_file_name, number_of_sensors)
 
     # Converting the pcap bag to a pointcloud2 bag
     roscore_proc = pcap_to_pointcloud(pcap_bag_name, json_file_name)
     roscore_proc.terminate()
-    subprocess.run("rostopic list", shell=True)
-    
 
     bag_to_pcd("./temp/demo-pc.bag")
 
-    print("It worked!")
+
+def main():
+    ''' Command line arguments:
+        
+        For single file conversions
+        single
+        pcap file name
+        json file name
+        number of sensors (16, 32, 64, 128)
+            This is usually part of the pcap name
+
+        For many different files
+        batch
+        directory containing all pcaps and jsons. Each pair must have the same name before the extensions
+        number of sensors (16, 32, 64, 128)
+            This is usually part of the pcap name
+    '''
+    conversion_mode = sys.argv[1]  # Either 'single' or 'batch' for the mode
+    
+    if conversion_mode == 'single':       
+        if len(sys.argv) < 5:
+                print("Must have appropriate arguments")
+                return 1
+        pcap_file_name = sys.argv[2]
+        json_file_name = sys.argv[3]
+        number_of_sensors = sys.argv[4]
+        
+        run_conversion(pcap_file_name, json_file_name, number_of_sensors)
+
+        print("Conversions done!")
+
+    elif conversion_mode == 'batch':
+        if len(sys.argv) < 4:
+                print("Must have appropriate arguments")
+                return 1
+        input_folder = sys.argv[2]
+        number_of_sensors = sys.argv[3]
+
+        # Gets a list of file names (not including extensions). If there is anything wrong, will return Null.
+        file_names = batch_get_file_names(input_folder)
+        if file_names == None:
+            print("File name error, exiting")
+
+        run_batch_conversions(input_folder, file_names, number_of_sensors)
+
+    else:
+        print("Please select a conversion mode as your first parameter (single/batch)")
+
     
 
 
 
 if __name__ == "__main__":
+    freeze_support()
     main()
